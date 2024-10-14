42b44e6324ec3e0234019608d9e78ca79f8d0eb3
andreacagnina <amirkal@libero.it>
2024-10-14 16:34:07 +0200

    [feat] scaffolding

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from 'axios';
+export default {
+  name: 'AppProjects',
+  data() {
+    return {
+
+    }
+  },
+  created() {
+
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style lang="scss"></style>
