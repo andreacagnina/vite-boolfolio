11d378983b79ec6bd6f80a02aab77e6c63d89848
andreacagnina <amirkal@libero.it>
2024-10-16 13:09:24 +0200

    [feat] mod shadowbox

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -8,30 +8,29 @@ export default {
 <template>
     <section class="d-flex justify-content-center align-items-center flex-column">
         <div class="box bg-dark">
-            <h1 class="display-4">Benvenuti nel mio Portfolio</h1>
+            <h1 class="display-4">Benvenuti</h1>
         </div>
-        <div class="blur-box bg-dark">
+        <div class="bg-box box bg-dark">
         </div>
     </section>
 </template>
 
 <style lang="scss" scoped>
-.blur-box {
+.box.bg-box {
     height: 70%;
-    width: 1200px;
-    text-align: center;
     background-image: url(../assets/animal-2028258_960_720.webp);
     background-repeat: no-repeat;
     background-size: contain;
     background-position: center;
     border-radius: 0 0 15px 15px;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    box-shadow: 7px 10px 6px rgba(0, 0, 0, 0.548);
 }
 
 .box {
     height: 15%;
-    width: 1200px;
+    width: 900px;
     background-color: rgba(53, 52, 52, 0.699);
+    box-shadow: 7px 12px 6px rgba(0, 0, 0, 0.548);
     border-radius: 15px 15px 0 0;
     text-align: center;
 
